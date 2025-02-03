49472ab7c39361d2b7a5dba0706e7a65d743b2e1
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-02-03 17:10:45 +0200

    some refactoring

--- main.py
+++ main.py
@@ -1,89 +1,93 @@
-# import questions from questions.json
 import json
 import random
 import time
 
-
-class bcolors:
+# Colors
+class colors:
     CORRECT = '\033[92m'
     INCORRECT = '\033[91m'
     NORMAL = '\033[0m'
-    BLUE = '\033[94m'
+    SCORE = '\033[94m'
 
+print("\nWelcome to the Computer Networks quiz!")
+print("You will be asked a series of multiple choice questions, and you must answer them correctly.")
+print("Write (in lowercase) all the corresponding letter/s of the answer/s you think is/are correct.")
 
-# import questions from questions.json
-print("Welcome to the quiz!")
-print(
-    "You will be asked a series of multiple choice question, and you must answer them correctly. Write with downcase all the corresponding letter of the answer you think is correct")
-print("For the answers that are not multiple choice, write the answer you think is correct")
-# store the questions in a dictionary
+# Store the questions in an array
 questions = []
 with open('questions.json', 'r', encoding='utf-8-sig') as f:
     for q in json.load(f):
         questions.append(q)
 
+print("\nTotal nr. of questions: " + str(len(questions)))
+
 while True:
-    # ask user how many questions they want to answer
-    # if user writes all, then provide all questions
-    user_input = input("How many questions do you want to answer?(write all if you want to answer all questions): ")
-    nr_of_questions = len(questions)
-    if user_input != "all":
+    # Ask user how many questions they want to answer
+    user_input = input("How many questions do you want to answer?\n-> ")
+    nr_questions = len(questions)
+    try:
         user_input = int(user_input)
-        if user_input < nr_of_questions:
-            nr_of_questions = user_input
+        if user_input <= nr_questions:
+            nr_questions = user_input
+    except ValueError:
+        print("Incorrect input, please try again...\n")
+        continue
 
-    user_score = 0
-    # remember the questions that have already been asked
-    indexes_of_questions = [i for i in range(len(questions))]
-    wrong_questions = []
-    for i in range(nr_of_questions):
-        print(bcolors.NORMAL + "Question " + str(i + 1) + "/" + str(nr_of_questions))
-        # wait 1 second before displaying next question
-        # time.sleep(1)
+    score = 0
+    indexes = [i for i in range(len(questions))]
+    mistakes = []
+    for i in range(nr_questions):
+        print(colors.NORMAL + "\n-- Question " + str(i + 1) + "/" + str(nr_questions) + " --")
 
-        # select a random question
-        index_of_question = random.choice(indexes_of_questions)
-        # remove the question from the list of questions that have not been asked
-        indexes_of_questions.remove(index_of_question)
+        # Select a random question
+        index = random.choice(indexes)
+        q = questions[index]
 
-        q = questions[index_of_question]
+        # Removes the selected question from the set of questions
+        # If user chooses to play again, the already answered questions won't get chosen again
+        questions.remove(q)
 
-        print(bcolors.NORMAL + q['question'])
-        # for each answer, display the answer
+        # Display the question
+        print(colors.NORMAL + q['question'])
+        
+        # Display the answer
         if q['answers'] != []:
-            # for each question that is multiple choice, display the answers, and in front of each one, display the corresponding character
-            # for example, the first answer will be a, the second will be b, etc.
+            # For each question display the answers, and in front of each one, display the corresponding character
+            # first answer will correspond to 'a', the second to 'b' etc.
             for i in range(len(q['answers'])):
                 print(chr(i + 97) + ") " + q['answers'][i])
-        # get user input
-        user_answer = input("Your answer: ")
-        # check if user input is correct
-        if user_answer == q['correct']:
-            print(bcolors.CORRECT + "Correct!")
-            user_score += 1
+        
+        # Get user input
+        answer = input("Your answer: ")
+        # Check user input
+        if answer == q['correct']:
+            print(colors.CORRECT + "Correct!")
+            score += 1
         else:
-            print(bcolors.INCORRECT + "Incorrect!")
-            print("The correct answer was " + q['correct'])
-            wrong_questions.append((q, user_answer))
+            print(colors.INCORRECT + "Incorrect!")
+            print("The correct answer was: " + q['correct'])
+            mistakes.append((q, answer))
 
-    # convert user score to percentage
-    user_score = user_score / nr_of_questions * 100
-    print(bcolors.BLUE + "You got " + str(user_score) + "%\n")
-    user_input = input(bcolors.NORMAL + "Do you want to review your mistakes?(y/n): ")
+    # Convert user score to percentage
+    score = score / nr_questions * 100
+    print(colors.SCORE + "You got " + str(score) + "%\n")
+
+    # Ask user to review mistakes
+    user_input = input(colors.NORMAL + "Do you want to review your mistakes?(y/n): ")
     if user_input == "y":
-        for (q, user_answer) in wrong_questions:
-            print(bcolors.NORMAL + q['question'])
+        for (q, answer) in mistakes:
+            print(colors.NORMAL + q['question'])
             for i in range(len(q['answers'])):
                 print(chr(i + 97) + ") " + q['answers'][i])
-            print("Your answer was " + user_answer)
-            print("The correct answer was " + q['correct'])
+            print("Your answer was " + colors.INCORRECT + answer)
+            print(colors.NORMAL + "The correct answer was: " + colors.CORRECT + q['correct'] + '\n')
 
-    # ask user if they want to play again
-    user_input = input("Do you want to play again?(y/n): ")
+    # Ask user if they want to play again
+    user_input = input(colors.NORMAL + "Do you want to play again? (y/n): ")
     if user_input == "n":
-        print("Thank you for playing!")
-        time.sleep(5)
+        print("You shall (definitely) pass!")
+        time.sleep(0.5)
         break
     else:
         print("Starting again...")
-        time.sleep(3)
+        time.sleep(1.5)
